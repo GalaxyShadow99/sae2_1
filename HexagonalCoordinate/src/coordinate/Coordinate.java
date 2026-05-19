ab6cf13c324dc8d66910d8856113898136aaa038
Victor Anger--Renault <hflkdfhg@oui>
2026-05-19 11:52:10 +0200

    fix complete

--- HexagonalCoordinate/src/coordinate/Coordinate.java
+++ HexagonalCoordinate/src/coordinate/Coordinate.java
@@ -1,144 +1,36 @@
 package coordinate;
 
-import java.security.InvalidParameterException;
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Coordinate {
-	public Point to2DCoordinate() {
-		return new Point(getX(), getY());
-	}
 
-	public Coordinate toDir(Mode mode, Direction direction) {
-		if (direction.equals(Direction.N)) {
+    public abstract Point to2DCoordinate();
+    
+    
+    public Coordinate toDir(Mode mode, Direction direction) {
+        if (direction.equals(Direction.N)) return N(mode);
+        if (direction.equals(Direction.NE)) return NE(mode);
+        if (direction.equals(Direction.NO)) return NO(mode);
+        if (direction.equals(Direction.E)) return E(mode);
+        if (direction.equals(Direction.O)) return O(mode);
+        if (direction.equals(Direction.S)) return S(mode);
+        if (direction.equals(Direction.SE)) return SE(mode);
+        if (direction.equals(Direction.SO)) return SO(mode);
+        return null;
+    }
 
-		} else if (direction.equals(Direction.NE)) {
-			return this.NE(mode);
-		} else if (direction.equals(Direction.NO)) {
-			return this.NO(mode);
-		} else if (direction.equals(Direction.E)) {
-			return this.E(mode);
-		} else if (direction.equals(Direction.O)) {
-			return this.O(mode);
-		} else if (direction.equals(Direction.S)) {
-			return this.S(mode);
-		} else if (direction.equals(Direction.SE)) {
-			return this.SE(mode);
-		} else if (direction.equals(Direction.SO)) {
-			return this.SO(mode);
-		}
-		return null;
-	}
+    public List<Coordinate> getNeighbors(Mode mode) {
+        return null;
+    }
 
-	public List<Coordinate> getNeighbors(Mode mode) {
-		return null;
-	}
+    public abstract List<Coordinate> between(Mode mode, Coordinate to) throws DifferentAxisException;
 
-	public List<Coordinate> beetween(Mode mode, Coordinate to) throws DifferentAxisException {
-		List<Coordinate> resultat = new ArrayList<>();
-		if (mode.equals(Mode.FLAT)) {
-			if ((this.getX() != to.getX()) || (this.getY() != to.getY())) {
-				throw new DifferentAxisException();
-			}
-		} else {
-			if ((this.getQ() != to.getQ()) && (this.getR() != to.getR()) && (this.getS() != to.getS())) {
-				throw new DifferentAxisException();
-			}
-		}
-
-		int pasQ = 0;
-		if (this.getQ() < to.getQ())
-			pasQ = 1;
-		else if (this.getQ() > to.getQ())
-			pasQ = -1;
-		int pasR = 0;
-		if (this.getR() < to.getR())
-			pasR = 1;
-		else if (this.getR() > to.getR())
-			pasR = -1;
-		int pasS = 0;
-		if (this.getS() < to.getS())
-			pasS = 1;
-		else if (this.getS() > to.getS())
-			pasS = -1;
-
-		int qActuel = this.getQ() + pasQ;
-		int rActuel = this.getR() + pasR;
-		int sActuel = this.getS() + pasS;
-
-		while (qActuel != to.getQ() || rActuel != to.getR() || sActuel != to.getS()) {
-			resultat.add(new CoordinateCube(qActuel, rActuel, sActuel));
-			qActuel += pasQ;
-			rActuel += pasR;
-			sActuel += pasS;
-		}
-		return resultat;
-	}
-
-	public Coordinate NO(Mode mode) {
-		if (mode.equals(Mode.FLAT)) {
-			throw new InvalidParameterException();
-		}
-		return new CoordinateCube(this.getQ(), this.getR(), this.getS() + 1);
-	}
-
-	public Coordinate NE(Mode mode) {
-		if (mode.equals(Mode.FLAT)) {
-			throw new InvalidParameterException();
-		}
-		return new CoordinateCube(this.getQ() + 1, this.getR(), this.getS());
-	}
-
-	public Coordinate E(Mode mode) {
-		if (mode.equals(Mode.POINTY)) {
-			throw new InvalidParameterException();
-		}
-		return new CoordinateDoubled(this.getX() + 1, this.getY());
-	}
-
-	public Coordinate O(Mode mode) {
-		if (mode.equals(Mode.POINTY)) {
-			throw new InvalidParameterException();
-		}
-		return new CoordinateDoubled(this.getX() - 1, this.getY());
-	}
-
-	public Coordinate N(Mode mode) {
-		if (mode.equals(Mode.POINTY)) {
-			throw new InvalidParameterException();
-		}
-		return new CoordinateDoubled(this.getX(), this.getY() + 1);
-	}
-
-	public Coordinate S(Mode mode) {
-		if (mode.equals(Mode.POINTY)) {
-			throw new InvalidParameterException();
-		}
-		return new CoordinateDoubled(this.getX(), this.getY() - 1);
-	}
-
-	public Coordinate SO(Mode mode) {
-		if (mode.equals(Mode.FLAT)) {
-			throw new InvalidParameterException();
-		}
-		return new CoordinateCube(this.getQ() - 1, this.getR(), this.getS());
-	}
-
-	public Coordinate SE(Mode mode) {
-		if (mode.equals(Mode.FLAT)) {
-			throw new InvalidParameterException();
-		}
-		return new CoordinateCube(this.getQ(), this.getR(), this.getS() - 1);
-	}
-
-	public abstract int getX();
-
-	public abstract int getY();
-
-	public abstract int getQ();
-
-	public abstract int getR();
-
-	public abstract int getS();
-
+    public abstract Coordinate NO(Mode mode);
+    public abstract Coordinate NE(Mode mode);
+    public abstract Coordinate E(Mode mode);
+    public abstract Coordinate O(Mode mode);
+    public abstract Coordinate N(Mode mode);
+    public abstract Coordinate S(Mode mode);
+    public abstract Coordinate SO(Mode mode);
+    public abstract Coordinate SE(Mode mode);
 }
--- HexagonalCoordinate/src/coordinate/CoordinateCube.java
+++ HexagonalCoordinate/src/coordinate/CoordinateCube.java
@@ -1,37 +1,100 @@
 package coordinate;
 
-public class CoordinateCube extends Coordinate{
-	private int q;
-	private int r;
-	private int s;
-	
-	public CoordinateCube(int q, int r, int s){
-		this.q = q;
-		this.r = r;
-		this.s = s;
-	}
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
 
-	public int getQ() {
-		return q;
-	}
+public class CoordinateCube extends Coordinate {
+    private int q;
+    private int r;
+    private int s;
+    
+    public CoordinateCube(int q, int r, int s) {
+        if (q + r + s != 0) {
+            throw new IllegalArgumentException("La somme de q, r et s doit être égale à 0");
+        }
+        this.q = q;
+        this.r = r;
+        this.s = s;
+    }
 
-	public int getR() {
-		return r;
-	}
+    public int getQ() { return q; }
+    public int getR() { return r; }
+    public int getS() { return s; }
 
-	public int getS() {
-		return s;
-	}
+    @Override
+    public Coordinate NO(Mode mode) {
+        return new CoordinateCube(this.q, this.r - 1, this.s + 1);
+    }
+
+    @Override
+    public Coordinate NE(Mode mode) {
+        return new CoordinateCube(this.q + 1, this.r - 1, this.s);
+    }
+
+    @Override
+    public Coordinate E(Mode mode) {
+        if (mode == Mode.FLAT) throw new InvalidParameterException("E n'accepte pas le mode FLAT");
+        return new CoordinateCube(this.q + 1, this.r, this.s - 1);
+    }
+
+    @Override
+    public Coordinate O(Mode mode) {
+        if (mode == Mode.FLAT) throw new InvalidParameterException("O n'accepte pas le mode FLAT");
+        return new CoordinateCube(this.q - 1, this.r, this.s + 1);
+    }
+
+    @Override
+    public Coordinate N(Mode mode) {
+        if (mode == Mode.POINTY) throw new InvalidParameterException("N n'accepte pas le mode POINTY");
+        return new CoordinateCube(this.q, this.r - 1, this.s + 1); 
+    }
+
+    @Override
+    public Coordinate S(Mode mode) {
+        if (mode == Mode.POINTY) throw new InvalidParameterException("S n'accepte pas le mode POINTY");
+        return new CoordinateCube(this.q, this.r + 1, this.s - 1); 
+    }
+
+    @Override
+    public Coordinate SO(Mode mode) {
+        return new CoordinateCube(this.q - 1, this.r + 1, this.s);
+    }
+
+    @Override
+    public Coordinate SE(Mode mode) {
+        return new CoordinateCube(this.q, this.r + 1, this.s - 1);
+    }
+
+    @Override
+    public List<Coordinate> between(Mode mode, Coordinate to) throws DifferentAxisException {
+        if (!(to instanceof CoordinateCube)) throw new InvalidParameterException();
+        CoordinateCube target = (CoordinateCube) to;
+        
+        if ((this.q != target.q) && (this.r != target.r) && (this.s != target.s)) {
+            throw new DifferentAxisException("Not on the same axis");
+        }
+
+        List<Coordinate> resultat = new ArrayList<>();
+        int pasQ = Integer.compare(target.q, this.q);
+        int pasR = Integer.compare(target.r, this.r);
+        int pasS = Integer.compare(target.s, this.s);
+
+        int qActuel = this.q + pasQ;
+        int rActuel = this.r + pasR;
+        int sActuel = this.s + pasS;
+
+        while (qActuel != target.q || rActuel != target.r || sActuel != target.s) {
+            resultat.add(new CoordinateCube(qActuel, rActuel, sActuel));
+            qActuel += pasQ;
+            rActuel += pasR;
+            sActuel += pasS;
+        }
+        return resultat;
+    }
 
 	@Override
-	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
+	public Point to2DCoordinate() {
+		return null;
 	}
 }
