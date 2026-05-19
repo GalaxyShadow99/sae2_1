082dc349a636a82ceac291b8e65f2bcf538d33e7
Victor Anger--Renault <hflkdfhg@oui>
2026-05-19 09:31:52 +0200

    methodes direction

--- HexagonalCoordinate/src/coordinate/Coordinate.java
+++ HexagonalCoordinate/src/coordinate/Coordinate.java
@@ -1,5 +1,6 @@
 package coordinate;
 
+import java.security.InvalidParameterException;
 import java.util.List;
 
 public abstract class Coordinate {
@@ -18,4 +19,61 @@ public abstract class Coordinate {
 	public List<Coordinate> beetween(Mode mode, Coordinate to){
 		return List<Coordinate>;
 	}
+	
+	public Coordinate NO(Mode mode) {
+		if (mode.equals(Mode.FLAT)){
+			throw new InvalidParameterException();
+		}
+		return new CoordinateCube(this.q,this.r, this.s+1);
+	}
+	
+	public Coordinate NE(Mode mode) {
+		if (mode.equals(Mode.FLAT)){
+			throw new InvalidParameterException();
+		}
+		return new CoordinateCube(this.q+1,this.r, this.s);
+	}
+	
+	public Coordinate E(Mode mode) {
+		if (mode.equals(Mode.POINTY)){
+			throw new InvalidParameterException();
+		}
+		return new CoordinateDoubled(this.x+1,this.y);
+	}
+	
+	public Coordinate O(Mode mode) {
+		if (mode.equals(Mode.POINTY)){
+			throw new InvalidParameterException();
+		}
+		return new CoordinateDoubled(this.x-1,this.y);
+	}
+	
+	public Coordinate N(Mode mode) {
+		if (mode.equals(Mode.POINTY)){
+			throw new InvalidParameterException();
+		}
+		return new CoordinateDoubled(this.x,this.y+1);
+	}
+	
+	public Coordinate S(Mode mode) {
+		if (mode.equals(Mode.POINTY)){
+			throw new InvalidParameterException();
+		}
+		return new CoordinateDoubled(this.x,this.y-1);
+	}
+	
+	public Coordinate SO(Mode mode) {
+		if (mode.equals(Mode.FLAT)){
+			throw new InvalidParameterException();
+		}
+		return new CoordinateCube(this.q-1,this.r, this.s);
+	}
+	
+	public Coordinate SE(Mode mode) {
+		if (mode.equals(Mode.FLAT)){
+			throw new InvalidParameterException();
+		}
+		return new CoordinateCube(this.q,this.r, this.s-1);
+	}
+	
 }
